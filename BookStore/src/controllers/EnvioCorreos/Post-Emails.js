73de0b515a6bf7e49ba8350e534fc373bc884f41
FT-37aDanielLuevano <130702268+FT-37aDanielLuevano@users.noreply.github.com>
2023-07-19 19:07:53 -0600

    Mercado libre Vuelve a funcionar

--- BookStore/src/controllers/EnvioCorreos/Post-Emails.js
+++ BookStore/src/controllers/EnvioCorreos/Post-Emails.js
@@ -1,25 +1,29 @@
 // Libreria a utilizar para el envio de correos automatico
 const nodemailer = require("nodemailer");
 
-// Dany trabajar en la coneccion de la data de usurio para llenar los campos de name ,from, to ,subjet
-
 const envioCorreo = async (result, res) => {
-  try {
-    const transporter = nodemailer.createTransport({
-      service: "gmail",
-      auth: {
-        user: process.env.USER_CORREO,
-        pass: process.env.USER_PASS_CORREO,
-      },
-      secure: true, // Habilitar la conexión segura
-    });
+    try {
+        const transporter = nodemailer.createTransport({
+            service: "gmail",
+            auth: {
+                user: process.env.USER_CORREO,
+                pass: process.env.USER_PASS_CORREO,
+            },
+            secure: true, // Habilitar la conexión segura
+        });
 
-    const name = `${result.name}`;
-    const mailOptions = {
-      from: "hoteldeveloperfull@gmail.com",
-      to: `${result.email}`,
-      subject: "Comprobante de pago",
-      html: `
+        const name = `${result.name}`;
+        const items = result.title.map((title, index) => ({
+            title: title,
+            quantity: result.quantity[index],
+            unit_price: result.unit_price[index],
+        }));
+
+        const mailOptions = {
+            from: "hoteldeveloperfull@gmail.com",
+            to: `${result.email}`,
+            subject: "Comprobante de pago",
+            html: `
       <head>
         <title>Recibo de Pago - E-Books Gracias por tu compra</title>
         <style>
@@ -47,23 +51,20 @@ const envioCorreo = async (result, res) => {
                 border-collapse: collapse;
                 margin-bottom: 20px;
                 table-layout: fixed; /* Fijar el ancho de las columnas */
-              }
-              
-              .table th,
-              .table td {
+            }
+            .table th,
+            .table td {
                 padding: 8px;
                 border: 1px solid #ccc;
                 word-wrap: break-word; /* Ajustar el texto automáticamente */
                 max-width: 150px; /* Máximo ancho de las celdas */
                 overflow: hidden; /* Ocultar contenido que excede el ancho máximo */
                 text-overflow: ellipsis; /* Agregar puntos suspensivos (...) para contenido truncado */
-              }
-              
-              .table th {
+            }
+            .table th {
                 background-color: #f0f0f0;
                 text-align: left;
-              }
-              
+            }
             .total {
                 text-align: right;
             }
@@ -80,65 +81,64 @@ const envioCorreo = async (result, res) => {
                 box-shadow: 0 0 5px rgba(0, 0, 0, 0.3);
             }
         </style>
-    </head>
-    <body>
+      </head>
+      <body>
         <div class="container">
-            <h1>Recibo de Pago</h1>
-           
-                 <p><strong>Gracias por tu compra en E-books</strong></p>
-                <p>Dirección Libreria, Ciudad</p>
-                <p>Teléfono: 81-23-43-43-45</p>
-                <p>Gracias port tu Compra ${name}</p>
-           
-            <h2>Detalles de la Compra</h2>
-            <table class="table">
-                 <thead>
+          <h1>Recibo de Pago</h1>
+          <p><strong>Gracias por tu compra en E-books</strong></p>
+          <p>Dirección Libreria, Ciudad Monterrey</p>
+          <p>Teléfono: 81-23-43-43-45</p>
+          <p>Gracias por tu Compra ${name}</p>
+          <h2>Detalles de la Compra</h2>
+          <table class="table">
+            <thead>
+              <tr>
+                <th>No.</th>
+                <th>Producto</th>
+                <th>Cantidad</th>
+                <th>Precio Unitario</th>
+                <th>Total</th>
+              </tr>
+            </thead>
+            <tbody>
+              ${items
+                    .map(
+                        (item, index) => `
                     <tr>
-                        <th>No.</th>
-                        <th>Orden de Pago</th>
-                        <th>Metodo de pago</th>
-                        <th>Tipo de pago</th>
-                        <th>Status de Pago</th>
-                        <th>Total</th>
+                      <td>${index + 1}</td>
+                      <td>${item.title}</td>
+                      <td>${item.quantity}</td>
+                      <td>$${item.unit_price}</td>
+                      <td>$${(item.quantity * item.unit_price).toFixed(2)}</td>
                     </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>${result.idpay}</td>
-                        <td>${result.order}</td>
-                        <td>${result.orderType}</td>
-                        <td>${result.operationType}</td>
-                        <td>${result.data_aprove}</td>
-                        <td>$${result.total_paid_amount}</td>
-                    </tr>
-                </tbody>
-            </table>
-            <div class="total">
-                <!-- Total a pagar -->
-            </div>
-    
-            <!-- Agregar la imagen -->
-            <div class="image-container">
-                <img src="https://th.bing.com/th/id/R.f54d2bb15ee1f6dea4c14bf2ca44e9ce?rik=XlqxAjggAgf%2b0g&riu=http%3a%2f%2fwww.writersblockbookstore.com%2fsites%2fwritersblockbookstore.com%2ffiles%2fBookstore+image+2017.jpg&ehk=KksPEtWr1lpM2zUm3h2O87075ImBAgViDTeJd%2fLdrSQ%3d&risl=&pid=ImgRaw&r=0">
-            </div>
+                  `
+                    )
+                    .join("")}
+            </tbody>
+          </table>
+          <div class="total">
+            <strong>Total a pagar: $${result.total_paid_amount}</strong>
+          </div>
+          <div class="image-container">
+            <img src="https://th.bing.com/th/id/R.f54d2bb15ee1f6dea4c14bf2ca44e9ce?rik=XlqxAjggAgf%2b0g&riu=http%3a%2f%2fwww.writersblockbookstore.com%2fsites%2fwritersblockbookstore.com%2ffiles%2fBookstore+image+2017.jpg&ehk=KksPEtWr1lpM2zUm3h2O87075ImBAgViDTeJd%2fLdrSQ%3d&risl=&pid=ImgRaw&r=0"/>
+          </div>
         </div>
-    </body>
+      </body>
       `,
-    };
+        };
 
-    transporter.sendMail(mailOptions, function (error, info) {
-      if (error) {
-        console.log(error);
-        return
-      } else {
-        console.log("Correo enviado: " + info.response);
-        return
-      }
-    });
-
-  } catch (error) {
-    throw new Error({ error: error.message });
-  }
+        transporter.sendMail(mailOptions, function (error, info) {
+            if (error) {
+                console.log(error);
+                return;
+            } else {
+                console.log("Correo enviado: " + info.response);
+                return;
+            }
+        });
+    } catch (error) {
+        throw new Error({ error: error.message });
+    }
 };
 
 module.exports = { envioCorreo };
