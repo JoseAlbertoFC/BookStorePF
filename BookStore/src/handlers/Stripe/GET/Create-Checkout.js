d59858c731e3d93b9a760d560ae1fefc555ab069
Juan Daniel Luevano Ruiz <daniel.luevanoui@uanl.edu.mx>
2023-07-29 00:32:04 -0600

    Stripe Revert

--- BookStore/src/handlers/Stripe/GET/Create-Checkout.js
+++ BookStore/src/handlers/Stripe/GET/Create-Checkout.js
@@ -1,10 +1,10 @@
 
 const {createSession} = require("../../../controllers/Stripe/Session-Stripe");
 const Checkout = async (req,res) => {
-    const { carrito, email, IdBook, name, userId }= req.body
+    const { items, email, idBook, user, userId }= req.body
   
   try{
-      const result = await createSession({ carrito, email, IdBook, name, userId })
+      const result = await createSession({ items, email, idBook, user, userId })
       if(result.id) throw new Error("No se pudo crear el pago en la base de datos")
     res.status(200).json(result)
   } catch (error) {
