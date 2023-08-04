56d16bb00c1ce5b8d92857d0aabf720968a46e92
OmarnLujan <127269393+OmarnLujan@users.noreply.github.com>
2023-08-04 20:42:25 -0300

    correccion

--- BookStore/src/handlers/Stripe/GET/Succes.js
+++ BookStore/src/handlers/Stripe/GET/Succes.js
@@ -16,7 +16,7 @@ const Succes = async (req,res) => {
     envioCorreo(dataPay)
   
 
-    res.redirect('http://localhost:3000/payment/sucessfulpay')
+    res.redirect('https://book-store-client-coral.vercel.app/payment/sucessfulpay')
   }catch(error){
     res.status(400).json({message:error.message})
   }
