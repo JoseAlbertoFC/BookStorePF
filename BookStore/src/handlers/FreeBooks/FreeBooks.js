abf606e490745787ed5b3047a36159a85e3dff2e
Juan Daniel Luevano Ruiz <daniel.luevanoui@uanl.edu.mx>
2023-08-05 14:13:54 -0600

    Creditos a Jose && Danny  URL DE PRODUCCION

--- BookStore/src/handlers/FreeBooks/FreeBooks.js
+++ BookStore/src/handlers/FreeBooks/FreeBooks.js
@@ -1,18 +1,46 @@
 
-const {envioCorreo} = require('../../controllers/EnvioCorreos/Send-Stripe/Post-Emails.js')
+const {envioCorreo} = require('../../controllers/EnvioCorreos/Send-Stripe/Post-Emails.js');
+const { newPay } = require('../../controllers/Pay-Controllers/POST/Post-Pay.js');
 
 
 const Freebook = async (req, res) => {
-
+    const result = req.body;
+    const dataFreebook = {
+        ip: 'null',
+        orderNumber: 'null',
+        metodo: 'Free',
+        currentOperation: 'null',
+        net_received_amount: 0.00,
+        amount: 0.00,
+        paymentStatus:'free',
+        email:result.email,
+        order:'free',
+        name:result.name,
+        idpay:'null',
+        total_paid_amount:0.00,
+        operationType:'free',
+        orderType:'free',
+        data_aprove:'Aprovado',
+        pqyment_method_option:'free',
+        userId: result.userId,
+        bookId:result.bookIds[0],
+        bookIds: result?.map((item) => item.bookIds),
+        bookTitle: result?.map((item) => item.bookTitle),
+        quantity:result?.map((item) => item.bookIds.length),
+        price: 0.00,
+        typeMoney: 'free',
+    
+    }
+    
     try {
-        const result = req.body;
-        
+        await newPay(dataFreebook);
         await envioCorreo(result, res);
         res.status(200).json({ message: "Correo enviado" });
+        
     } catch (error) {
         res.status(500).json({ message: "Error al enviar el correo" });
     }
-
+    
 }
 
 module.exports = { Freebook }
