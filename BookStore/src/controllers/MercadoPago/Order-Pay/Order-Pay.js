1d144ec6372bdc40e69979acdf59806c6431e350
JoseAlbertoFC <fuenmayorcoa@gmail.com>
2023-08-08 20:20:48 -0300

    pasarelas lloronas

--- BookStore/src/controllers/MercadoPago/Order-Pay/Order-Pay.js
+++ BookStore/src/controllers/MercadoPago/Order-Pay/Order-Pay.js
@@ -5,21 +5,21 @@ const { ACCES_TOKEN } = process.env;
 
 // Esta funcion genera una orden de pago recibe el carrito y el userid junto con el bokingid para poder registrar los datos de la reserva que el cliente esta haciendo.
 const ORDER_PAY = async ({ carrito, name, email, IdBook, typeMoney, userId }) => {
-  console.log(carrito, "Soy tu sexy Carrito")
-    mercadopago.configure({
-        access_token: ACCES_TOKEN,
-    });
-    const items = carrito.map((item) => ({
-        IdBook: item.IdBook,
-        title: item.nombre,
-        unit_price: item.precio,
-        currency_id: item.typeMoney,
-        quantity: item.cantidad,
-        category_id: item.IdBook,
-        description: item.description,
-        pdfLink: item.pdfLink
-    }));
-    
+  mercadopago.configure({
+    access_token: ACCES_TOKEN,
+  });
+  const items = carrito.map((item) => ({
+    IdBook: item.IdBook,
+    title: item.nombre,
+    unit_price: item.precio,
+    currency_id: item.typeMoney,
+    quantity: item.cantidad,
+    category_id: item.IdBook,
+    description: item.description,
+    pdfLink: item.pdfLink
+  }));
+  console.log(carrito.pdfLink, "Soy tu sexy Carrito")
+  
     
   // En este apartado en desarrollo tienes que correr el ngrok en una consola aparte y copiar el url que te brinda.
     const notificationURL ="https://bookstorepf-production.up.railway.app/webhook-pago";
