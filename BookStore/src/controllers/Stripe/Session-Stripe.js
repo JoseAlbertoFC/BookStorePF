1fbe9df17780a2dae83c6417ad3b7dc998ba7b55
Juan Daniel Luevano Ruiz <daniel.luevanoui@uanl.edu.mx>
2023-08-09 18:26:21 -0600

    Creditos a Jose && Danny  link PDF

--- BookStore/src/controllers/Stripe/Session-Stripe.js
+++ BookStore/src/controllers/Stripe/Session-Stripe.js
@@ -9,6 +9,7 @@ const stripe = new Stripe(Sripe_SECRET);
 
 const createSession = async ({ items, email, idBook, user, userId }) => {
 	console.log(items)
+	
   try {
 	  const lineItems = items.map((item) => ({
 	  price_data: {
@@ -25,14 +26,25 @@ const createSession = async ({ items, email, idBook, user, userId }) => {
 	  }));
 	  const idBooks = items.map((item) => item.idBook);
 	  const pdfLink = items.map((item) => (item.pdfLink))
+	  const linksDatabase = {};
+	  const linkPDF = {};
+
+idBooks.forEach(link => {
+    const id = shortid.generate();
+    linksDatabase[id] = link;
+});
+pdfLink.forEach(link => {
+    const id = shortid.generate();
+    linksDatabase[id] = link;
+});
 	  const session = await stripe.checkout.sessions.create({
 		  line_items: lineItems,
 		  mode: "payment",
 		  success_url: "https://bookstorepf-production.up.railway.app/success?idBook=" + idBook + "&user=" + user + "&email=" + email + "&session_id={CHECKOUT_SESSION_ID}",
 		  cancel_url: "https://bookstorepf-production.up.railway.app/cancel?user=" + user + "&email=" + email,
 		  metadata: {
-			  idBooks: idBooks.join(),
-			  pdfLink: pdfLink.join()
+			idBooks: Object.keys(linksDatabase).join(),
+			pdfLink: Object.keys(linksDatabase).join()
 			  
 		  },
 	});
